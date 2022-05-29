6ec3c7f7e2d70be6812b4bf2fbde7fe994771869
Liidia Laada <liidia.laada@gmail.com>
2022-05-29 12:47:57 +0300

    Post form, query params, path variables.

--- src/main/java/com/jrp/demo/web/HelloController.java
+++ src/main/java/com/jrp/demo/web/HelloController.java
@@ -1,15 +1,43 @@
 package com.jrp.demo.web;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greeting")
 public class HelloController {
 
-	@RequestMapping(value="/basic", method = RequestMethod.GET)
+	@RequestMapping("/basic")
 	public String sayHello() {
 		return "<h1>Hello</h1>";
 	}
-}
+	
+	@RequestMapping("/proper")
+	public String sayProperHello() {
+		return "<h1>Hello there, how are you?</h1>";
+	}
+	
+	@RequestMapping("/user_entry")
+	public String userForm() {
+		return "<form action=\"/greeting/user_greeting\" method=\"POST\">\r\n"
+				+ "  <label for=\"fname\">First name:</label><br>\r\n"
+				+ "  <input type=\"text\" id=\"fname\" name=\"fname\"><br>\r\n"
+				+ "  <label for=\"lname\">Last name:</label><br>\r\n"
+				+ "  <input type=\"text\" id=\"lname\" name=\"lname\"><br><br>\r\n"
+				+ "  <input type=\"submit\" value=\"Submit\">\r\n"
+				+ "</form> ";
+	}
+	
+	@RequestMapping(value="/user_greeting", method = RequestMethod.POST)
+	public String printUserGreeting(@RequestParam String fname,@RequestParam String lname) {
+		return "Hello there, " + fname + " " + lname;
+	}
+	
+	@RequestMapping(value = "/orders/{id}", method = RequestMethod.GET)
+	public String getOrder(@PathVariable String id) {
+		return "Order ID: " + id;
+	}
+} 
